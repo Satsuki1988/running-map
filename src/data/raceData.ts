b811aa2b301d99d7f8653adcaca77679e64a6978
Satsuki1988 <satsuki1988@outlook.jp>
2026-09-13 16:11:33 +0900

    サンプルレースデータ修正

--- src/data/raceData.ts
+++ src/data/raceData.ts
@@ -1,1 +1,13 @@
- export const raceData = {};
+export interface Race {
+  race: string;
+  date: string;
+  time: string;
+  distance: string;
+  type: string;
+  garminUrl: string;
+}
+
+export const raceData: Record<
+  string,
+  Race[]
+> = {};
