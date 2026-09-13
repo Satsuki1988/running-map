af0c2e4f71b23ace222bf5f71e7e55aa39a0803b
Satsuki1988 <satsuki1988@outlook.jp>
2026-09-13 16:02:10 +0900

    サンプルレースデータ削除

---
+++ src/data/raceData_backup.ts
@@ -0,0 +1,42 @@
+ export const raceData = {
+    北海道: [
+      {
+        race: "北海道マラソン2024",
+        date: "2024-08-25",
+        time: "03:32:10",
+        distance:42.195,
+        type:"full",
+        garminUrl: "",
+      },
+      {
+        race: "北海道マラソン2025",
+        date: "2025-08-31",
+        time: "03:28:15",
+        distance:42.195,
+        type:"full",
+        garminUrl: "",
+      },
+    ],
+
+    東京都: [
+      {
+        race: "東京マラソン2025",
+        date: "2025-03-02",
+        time: "03:35:20",
+        distance:42.195,
+        type:"full",
+        garminUrl: "",
+      },
+    ],
+
+    大阪府: [
+      {
+        race: "大阪マラソン2025",
+        date: "2025-02-24",
+        time: "03:41:10",
+        distance:42.195,
+        type:"full",
+        garminUrl: "",
+      },
+    ],
+  };
